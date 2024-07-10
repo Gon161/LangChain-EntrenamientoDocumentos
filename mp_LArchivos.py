55aa4ff8075a4f06e35162ebaefcb495f33f950d
GON161 <82066172+Gon161@users.noreply.github.com>
2024-07-10 17:15:23 -0600

    Update mp_LArchivos.py

--- mp_LArchivos.py
+++ mp_LArchivos.py
@@ -1,3 +1,13 @@
+""""
+Documentacion de pgvector 
+https://github.com/pgvector/pgvector
+
+Videos de referencia 
+https://www.youtube.com/@saxsabigdata1400
+
+
+"""
+
 
 import io
 from langchain.text_splitter import RecursiveCharacterTextSplitter
@@ -27,50 +37,65 @@ def ProcesarSolicitud(sMetodo,sParametros):
     config = configparser.ConfigParser()
     config.read('config.ini', encoding='utf-8')
     COLLECTION_NAME = ""
+    #Cadena de conexion a postgresql con el plugin pgvector 
     CONNECTION_STRING = config['AppSettings']['CONNECTION_STRING']
 
     match (sMetodo):
         case "PDF":
-            sNombreDocumento, pdf_codificado, sNombrePDF = sParametros.split('|')
+            # Nombre de la empres, pdf en base 64, Nombre del PDF
+            sNombreEmpresa, pdf_codificado, sNombrePDF = sParametros.split('|')
+            # decodificar el pdf
             pdf_decodificado = base64.b64decode(pdf_codificado)
-            COLLECTION_NAME = sNombreDocumento
+            # identificador del documento 
+            COLLECTION_NAME = sNombreEmpresa
+            #cargar el pdf en memoria para su uso
             pdf_stream = io.BytesIO(pdf_decodificado)
             doc = fitz.open(stream=pdf_stream, filetype="pdf")
-
+            #variable que almacena del texto del documento 
             ggTXTCompleto = ""
 
-            # Iterate through each page and extract text
+            # ciclo que pasa por cada hoja 
             for page_num in range(doc.page_count):
+                #obtener la pagina a utilizar 
                 page = doc[page_num]
+                #obtener todo el texto de la pagina 
                 ggTXTCompleto += page.get_text()
+                #obtener todas la imagenes 
                 images = page.get_images(full=True)
+                #obtener todas las tablas
                 tabs = page.find_tables()
-                
+                #recorrer las tablas encontradas
                 for table_index, tab in enumerate(tabs):
+                    #pasar la tabla a una tabla de pandas 
                     df = tab.to_pandas()
+                    #generar salto de linea
                     ggTXTCompleto += "\n\n"
-                    
+                    #obtener el cabezado de cada tabla 
                     column_headers = [str(col).replace('\n', ' ') for col in df.columns]
+                    #generar una separacion con el simbolo ~
                     ggTXTCompleto += "~".join(column_headers) + "\n"
-
+                    #recorrer cada fila de la tabla y realizar su seperacion de ~ 
                     for index, row in df.iterrows():
                         processed_row = [str(cell).replace('\n', ' ') for cell in row]
                         ggTXTCompleto += "~".join(processed_row) + "\n"
                         ggTXTCompleto += ""
                 ggTXTCompleto += "\n\n"
-
+            #cerrar documento 
             doc.close()
-
+            #se pasa el texto de nuevo a un documento para se procesado por el plugin 
             text_splitter = CharacterTextSplitter()
             texts = text_splitter.split_text(ggTXTCompleto)
             docs = [Document(page_content=t) for t in texts]
 
             load_dotenv()
-            
+            #realizar el separado de parrafos 
             text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=20,length_function=len,is_separator_regex=False,)
             texts = text_splitter.split_documents(docs)
-            embeddings = OllamaEmbeddings(model="llama2")
+            #inicializar el modelo para el procesamiento de lenguaje natural
+            embeddings = OllamaEmbeddings(model="llama3")
+            #vectorizar los parrafos para guardar la informacion en la base de datos
             doc_vectors = embeddings.embed_documents([t.page_content for t in texts[:5]])
+            #registrar los vectores
             db = PGVector.from_documents(
                 embedding=embeddings,
                 documents=texts,
@@ -79,19 +104,24 @@ def ProcesarSolicitud(sMetodo,sParametros):
             )
             return "Proceso Terminado"
         case "PreguntaDocumento":
+            #Pregunta, Identificador del documento
             pregunta, COLLECTION_NAME= sParametros.split('|')
-            embeddings = OpenAIEmbeddings()
+            #iniciar el modelo para el procesamiento del lenguaje natural 
+            embeddings =OllamaEmbeddings(model="llama3")
+            #realizar la conexion a la base de datos vectorial 
             db = PGVector.from_documents(
                     embedding=embeddings,
                     documents="",
                     collection_name=COLLECTION_NAME,
                     connection_string=CONNECTION_STRING
                 )
-            # similar = db.similarity_search_with_score(query, k=3)
+            # configurar el retriever, extraer las 3 vectores mas similares a la pregunta 
             retriever = db.as_retriever(search_kwargs={'k': 3})  # default 4
-   
+            # inicializar el RetrievalQA
             qa_chain = RetrievalQA.from_chain_type(
-                llm=OpenAI(),chain_type="stuff", retriever=retriever, return_source_documents=True
+                llm=Ollama("llama3"),chain_type="stuff", retriever=retriever, return_source_documents=True
             )
+            #generar la respuesta procesada por el modelo con los vectores cercanos a la pregunta 
             result = qa_chain({"query": pregunta})
+            #retornar la respuesta 
             return result["result"]
